3b1f1378ad325096b824aa5e2a25a924dc70eb76
pskpsk <pikkilisateeshkumar@gmail.com>
2026-06-10 21:41:15 +0530

    Delete js/storage.js

--- js/storage.js
+++
@@ -1,4 +0,0 @@
-function getCart(){return JSON.parse(localStorage.getItem('cart')||'[]');}
-function saveCart(c){localStorage.setItem('cart',JSON.stringify(c));}
-function getOrders(){return JSON.parse(localStorage.getItem('orders')||'[]');}
-function saveOrders(o){localStorage.setItem('orders',JSON.stringify(o));}
